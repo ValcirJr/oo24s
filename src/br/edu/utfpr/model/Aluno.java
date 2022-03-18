4089bbb18b94de963b25bba4eac14deb9add4660
vbalbino <valcir.balbinottijunior@nttdata.com>
2022-03-18 19:32:41 -0300

    Aula 18-03 pt.1

--- src/br/edu/utfpr/model/Aluno.java
+++ src/br/edu/utfpr/model/Aluno.java
@@ -1,13 +1,36 @@
 package br.edu.utfpr.model;
 
 import br.edu.utfpr.sql.CreateTableHelper;
+import lombok.*;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Aluno extends Model{
 
     private String nome;
+    private String telefone;
+    private String email;
+    private LocalDate dataNascimento;
 
     @Override
-    public CreateTableHelper createTable() {
-        return null;
+    public CreateTableHelper generateCreateTableSQL() {
+        String createTable = "" +
+                "CREATE TABLE IF NOT EXISTS aluno (" +
+                    "id SERIAL PRIMARY KEY, " +
+                    "nome VARCHAR(50) NOT NULL, " +
+                    "telefone VARCHAR(20) NOT NULL, " +
+                    "email VARCHAR(70) NOT NULL, " +
+                    "dataNascimento DATE NOT NULL);";
+
+
+        String dropTable = "" +
+                "DROP TABLE IF EXISTS aluno";
+
+        return new CreateTableHelper(createTable, dropTable);
     }
 }
--- src/br/edu/utfpr/model/Model.java
+++ src/br/edu/utfpr/model/Model.java
@@ -1,13 +1,41 @@
 package br.edu.utfpr.model;
 
+import br.edu.utfpr.database.ConnectDataBase;
 import br.edu.utfpr.sql.CreateTableHelper;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Getter
 public abstract class Model {
 
     private int id;
 
-    public abstract CreateTableHelper createTable();
+    public abstract CreateTableHelper generateCreateTableSQL();
+
+    public void createTable() {
+        try {
+            Connection conn = ConnectDataBase.createConnections();
+
+            PreparedStatement psCreate = conn.prepareStatement(
+                    generateCreateTableSQL().getCreateTable());
+            PreparedStatement psDrop = conn.prepareStatement(
+                    generateCreateTableSQL().getDropTable());
+
+            psDrop.executeUpdate();
+            psCreate.executeUpdate();
+
+            psCreate.close();
+            psDrop.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO CRIAR TABELA");
+            e.printStackTrace();
+        }
+    }
 
 }
